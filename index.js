ad121632b6bcf7f896bd38ec07264f243eb7cf1d
amiedawn <amie.d.carroll@gmail.com>
2021-11-16 02:27:47 -0700

    numbers right

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ function createTable() {
     // 10 columns
     for (let j=0; j<10; j++) {
       let cell = row.insertCell(j);
-      cell.innerHTML =(i+1); //start with 1 instead of 0
+      cell.innerHTML = ((i*10) + j + 1); //start with 1 instead of 0
       cell.className = "item";
     }
+
   }
 };
