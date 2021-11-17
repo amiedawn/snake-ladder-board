ccbe50c09af4c60e39b4ababcbb587f3633c984a
amiedawn <amie.d.carroll@gmail.com>
2021-11-17 07:25:55 -0700

    die functionality working

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const showDie = document.querySelector('.die-image');
+const rollDie = document.querySelector('.btn-roll');
+
 function createTable() {
   let table = document.getElementById("myTable");
   
@@ -14,3 +17,45 @@ function createTable() {
     }
   }
 };
+
+
+function makeMove() {
+  // for (let i = images.length; i--;) {
+  //   images[i].addEventListener('click', change);
+  // }
+
+  // random # between 1 and 6
+  let rollValue = Math.floor(Math.random() * 6) + 1;
+  console.log(rollValue);
+
+  //let images = document.querySelector("die-images");
+
+  // display die image
+  showDie.style.display = "block";
+  showDie.src = "./assets/images/" + rollValue + ".png";
+  console.log(showDie.src);
+
+  // based on the result of the die roll, display an image that matches
+  // switch (rollValue) {
+  //   case 0:
+  //     makeMove("assets/images/1.png");
+  //     break;
+  //   case 1:
+  //     makeMove("assets/images/2.png");
+  //     break;
+  //   case 2:
+  //     makeMove("assets/images/3.png");
+  //     break;
+  //   case 3:
+  //     makeMove("assets/images/4.png");
+  //     break;
+  //   case 4:
+  //     makeMove("assets/images/5.png");
+  //     break;
+  //   case 5:
+  //     makeMove("assets/images/6.png");
+  //     break;
+  // }
+};
+
+rollDie.addEventListener('click', makeMove);
